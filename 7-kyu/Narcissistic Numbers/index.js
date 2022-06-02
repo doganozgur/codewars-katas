5ac09ae676bd0ba54a4cd0a90c678c01edddd550
Dogan <doganozgur034@gmail.com>
2022-06-02 15:20:23 +0300

    added new katas

---
+++ 7-kyu/Narcissistic Numbers/index.js
@@ -0,0 +1,21 @@
+/*
+A Narcissistic Number is a number of length l in which the sum of its digits to the power of l is equal to the original number. If this seems confusing, refer to the example below.
+
+Ex: 153, where l = 3 ( the number of digits in 153 )
+13 + 53 + 33 = 153
+
+Write a function that, given n, returns whether or not n is a Narcissistic Number.
+*/
+
+function isNarcissistic(n) {
+    const strNumber = n.toString();
+    const exponent = strNumber.length;
+    let arrayValue = strNumber.split('');
+    let sum = 0;
+    arrayValue.map((number) => {
+        sum += parseInt(number)**exponent
+    })
+    return sum == n ? true : false;
+}
+
+console.log(isNarcissistic(153)); // true
---
+++ 7-kyu/Sort by Last Char/index.js
@@ -0,0 +1,13 @@
+/*
+Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
+
+If two words have the same last letter, they returned array should show them in the order they appeared in the given string.
+
+All inputs will be valid.
+*/
+
+function last(x){
+    return x.split(' ').map(word => word).sort((a, b) => a.charCodeAt(a.length - 1) - b.charCodeAt(b.length - 1));
+}
+
+console.log(last("Ne mutlu Türküm diyene."));
